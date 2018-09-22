ff1fc4aba88717dcada65e837a8003a268249d52
Connor Svrcek <csvrcek@umich.edu>
2018-09-22 13:27:43 -0400

    Prep

---
+++ career/Control/CareerFairViewController.swift
@@ -0,0 +1,18 @@
+//
+//  CareerFairViewController.swift
+//  career
+//
+//  Created by Connor Svrcek on 9/22/18.
+//  Copyright © 2018 Connor Svrcek. All rights reserved.
+//
+
+import UIKit
+
+class CareerFairViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+}
