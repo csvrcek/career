dc6bb6de58d7162750bfb5fad45b64c2b325debe
Connor Svrcek <csvrcek@umich.edu>
2018-09-22 20:55:22 -0400

    Firestore setup

--- career/Control/CareerFairTableViewController.swift
+++ career/Control/CareerFairTableViewController.swift
@@ -9,38 +9,88 @@
 import UIKit
 import Firebase
 
-
 class CareerFairTableViewController: UITableViewController {
     
-    let fairs = Array<Fair>()
+    private var fairs = Array<Fair>()
+    
+    var docRef: DocumentReference!
+    
+    let refresh = UIRefreshControl()
+    
+    let fairID = "cellID"
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = UIColor.white
         setupNavigation()
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: fairID)
+        
+        // Firestore setup
+        docRef = Firestore.firestore().document("CareerFair/r1i802xAhNxJg7BrdYbu")
+        
+        fetchFromFirstore()
     }
     
+    // Setup navigation controller
     func setupNavigation() {
         navigationItem.title = "Career Fairs"
         navigationController?.navigationBar.prefersLargeTitles = true
+    }
+    
+    // Setup refresh controller
+    func setupRefresh() {
+        // Add Refresh Control to CollectionView
+        if #available(iOS 10.0, *) {
+            tableView.refreshControl = refresh
+        } else {
+            tableView.addSubview(refresh)
+        }
+        
+        refresh.addTarget(self, action: #selector(fetchFromFirstore), for: .valueChanged)
+    }
+    
+    // Fetch from Firestore (from https://www.youtube.com/watch?v=rvxYRm6n_NM)
+    @objc func fetchFromFirstore() {
+        docRef.getDocument { (docSnapshot, error) in
+            guard let docSnapshot = docSnapshot, docSnapshot.exists else {return}
+            let careerFairsData = docSnapshot.data()
+            let latestName = (careerFairsData!["careerFairName"] as! String)
+            let newFair = Fair()
+            newFair.name = latestName
+            self.fairs.append(newFair)
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
+        }
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return fairs.count
     }
     
+    func printFairs() {
+        print("fairs = ")
+        for i in fairs {
+            print("\(i.name), ")
+        }
+    }
+    
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        //navigationController?.pushViewController(CompanyTilesViewController(fair_in: fairs[indexPath]), animated: true)
+        printFairs()
+        navigationController?.pushViewController(CompanyTilesViewController(fair_in: fairs[indexPath.row]), animated: true)
     }
     
     override func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
+        return fairs.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let fair = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: fairID, for: indexPath)
+        let fair = fairs[indexPath.row]
+        cell.textLabel?.text = fair.name
         
+        print("indexPath.row = \(indexPath.row)")
         
-        return fair
+        return cell
     }
 }
